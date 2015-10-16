7686daa890757b138d5e795ece103cb9ff65aca0
Nick Joebgen <nick.joebgen@gmail.com>
2015-10-16 15:30:27 -0500

    Merge remote-tracking branch 'refs/remotes/origin/master' into Level-Design
    Conflicts:
    	Assets/Resources/Prefabs/Hallways/Hallway10_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway11_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway12_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway13_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway14_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway15_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway16_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway17_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway18_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway1_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway2_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway3_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway4_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway5_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway6_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway7_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway8_NJ.prefab
    	Assets/Resources/Prefabs/Hallways/Hallway9_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room11_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room13_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room15_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room16_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room17_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room18_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room19_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room20_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room21_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room22_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room23_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room25_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room27_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room28_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room29_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room30_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room3_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room6_NJ.prefab
    	Assets/Resources/Prefabs/Rooms/Room7_NJ.prefab

---
+++ Assets/Scripts/Generation/MapModule.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapModule : MonoBehaviour
+{
+    public enum Type
+    {
+        Room, Hall, Cap
+    };
+
+    public Type moduleType;
+
+    private RoomConnection[] connectors;
+
+    void Start()
+    {
+        if (connectors.Length == 0)
+        {
+            Debug.Log("Highly Probable Error: Each module should have at least one connector. Make sure the connector is a child of the parent.");
+        }
+        else if (moduleType == Type.Cap)
+        {
+            if (connectors.Length != 1)
+            {
+                Debug.Log("Probable Error: Caps are dead-ends to halls or cover up an open connector, and are thus intended to have only one connection.");
+            }
+        }
+    }
+
+    public RoomConnection[] GetConnectors()
+    {
+        if (connectors == null)
+        {
+            connectors = GetComponentsInChildren<RoomConnection>();
+        }
+        return connectors;
+    }
+}
